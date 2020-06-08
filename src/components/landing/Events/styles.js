e9ea69e692a44d5098ef410181e9dd9aa900eb19
natesabrown <nateabrown@icloud.com>
2020-06-08 14:08:02 -0500

    Add Event Section

--- src/components/landing/Events/styles.js
+++ src/components/landing/Events/styles.js
@@ -4,18 +4,41 @@ export const Wrapper = styled.div`
   padding: 2rem 0;
 `;
 
+export const SmallWrapper = styled.div`
+  padding: 1rem 0;
+`;
+
+export const EventsHeader = styled.div`
+  text-align: center;
+  h2 {
+    margin: 0;
+    margin: 0 auto;
+    color:white;
+    background-color: #6FACD5;
+    padding: 1em;
+    margin-bottom: 2rem;
+    border-radius: 2rem;
+    min-width: 3em;
+    display: inline-block;
+  }
+  
+`;
+
 export const Grid = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   grid-template-rows: 8fr;
   gap: 1.2rem 1.2rem;
+  p {
+    margin-top: 2em;
+  }
 
   @media (max-width: 960px) {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (max-width: 680px) {
+  @media (max-width: 800px) {
     grid-template-columns: 1fr;
   }
 `;
@@ -25,6 +48,7 @@ export const Item = styled.div`
   height: 100%;
   overflow: hidden;
   box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.11);
+  color: inherit;
 
   h4 {
     color: #212121;
@@ -35,27 +59,37 @@ export const Item = styled.div`
   }
 `;
 
-export const Content = styled.div`
-  padding: 1rem 0;
-`;
+export const AttributeItem = styled.div`
+margin-bottom: 12px;
 
-export const Stats = styled.div`
-  display: flex;
-  align-items: center;
+img {
+  width: 14px;
+  margin-right: 12px;
+  margin-bottom: 0px;
+}
+`
 
-  div {
-    display: flex;
-    &:first-child {
-      margin-right: 0.5rem;
-    }
+export const EventImage = styled.div`
+height: 368px;
+text-align: center;
+position: relative;
 
-    img {
-      margin: 0;
-    }
+img {
+  max-width: none;
+  height: inherit;
+  transition: height 0.5s ease-in-out, filter 0.5s ease-in-out;
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%) 
+}
 
-    span {
-      color: #000;
-      margin-left: 0.5rem;
-    }
-  }
-`;
+img:hover {
+  height: 280px;
+  filter: brightness(50%);
+}
+
+}
+`
