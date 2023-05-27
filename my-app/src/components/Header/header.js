f7fe05ed54403af88b32f7f130258588e6ca9b32
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-05-26 21:22:56 -0500

    updates to HEADER component

---
+++ my-app/src/components/Header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css'
+import Nav from '../Nav/nav';
+
+export default function Header({setActiveNavStr}) {
+  
+  return (
+    <section id="Header">
+      <Nav setActiveNavStr={setActiveNavStr} />
+      <h1>Code For Dallas</h1>
+    </section>
+  );
+  
+}
