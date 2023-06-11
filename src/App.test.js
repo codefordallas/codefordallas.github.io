504e8e9f3c6474262eaef82c67e698bef33fe729
jhoover4 <jordan@hoovermld.com>
2023-06-10 21:28:38 -0500

    Revert app test from commented out.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
-//
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+ /* eslint-disable */
+ // TODO: Turn back on eslint when this file is more ready to go.
+
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
